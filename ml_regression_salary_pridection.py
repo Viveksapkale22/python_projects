374258bce4f94fb088ce960204cd82c53cbda96a
Viveksapkale22 <162103595+Viveksapkale22@users.noreply.github.com>
2024-06-21 21:24:25 +0530

    Create ml_regression_salary_pridection.py
    provide salary predication on the bases of experience  with high accuracy rate of percentages . this is the regression problem (continuous value ) . we are use the linear regression for solving the  problems also we solve such problem using  knn model , tree , ensemble , etc.

---
+++ ml_regression_salary_pridection.py
@@ -0,0 +1,25 @@
+import pandas as pd
+salary = pd.read_csv('https://github.com/YBIFoundation/Dataset/raw/main/Salary%20Data.csv')
+
+y = salary['Salary']
+x = salary[['Experience Years']]
+salary.shape
+
+from sklearn.model_selection import train_test_split
+
+x_tarin , x_test , y_train , y_test = train_test_split(x,y,train_size = 0.7 , random_state = 2529)
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+
+model.fit(x_tarin,y_train)
+
+model.predict(x_test)
+
+from sklearn.metrics import mean_absolute_percentage_error
+error = mean_absolute_percentage_error(y_test,model.predict(x_test))
+
+print(100-(100*error),"% of the accuracy of the salary prediction ml model")
+print(x_test)
+print("this is the prediction values above table : ",model.predict(x_test))
+
